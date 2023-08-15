d77e68d7805dae66d9c4ed8cd2cb9e8f8b0474c2
Surya <82582201+SuryaCS719@users.noreply.github.com>
2023-08-15 19:56:06 +0530

    Create sudokuSolver.cpp

---
+++ sudokuSolver.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+bool is_valid(int board[9][9], int row, int col, int num) {
+    for (int i = 0; i < 9; i++) {
+        if (board[row][i] == num || board[i][col] == num) {
+            return false;
+        }
+    }
+
+    int start_row = 3 * (row / 3);
+    int start_col = 3 * (col / 3);
+    for (int i = start_row; i < start_row + 3; i++) {
+        for (int j = start_col; j < start_col + 3; j++) {
+            if (board[i][j] == num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool solveSudoku(int board[9][9]) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (board[i][j] == 0) {
+                for (int num = 1; num <= 9; num++) {
+                    if (is_valid(board, i, j, num)) {
+                        board[i][j] = num;
+                        if (solveSudoku(board)) {
+                            return true;
+                        }
+                        board[i][j] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    int board[9][9];
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    bool ans = solveSudoku(board);
+    if (ans) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
